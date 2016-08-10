7619cb424d5facfd2fb887634667f0dbb4c3a14b
Mathias Jakobsen <mathiasjakobsen@me.com>
2016-08-10 10:11:18 +0200

    Adds different responses to debtor procy methods.

--- lib/economic/proxies/debtor_proxy.rb
+++ lib/economic/proxies/debtor_proxy.rb
@@ -21,33 +21,46 @@ module Economic
       build_entities_from_handles(:debtor_contact, handles)
     end
 
-    def get_invoices(debtor_handle)
+    def get_invoices(debtor_handle, format = :entitites)
       handles = fetch_handles(:get_invoices, debtor_handle)
-      build_entities_from_handles(:invoice, handles)
+      send("build_#{format}_array_from_handles", *[:invoice, handles])
     end
 
-    def get_orders(debtor_handle)
+    def get_orders(debtor_handle, format = :entitites)
       handles = fetch_handles(:get_orders, debtor_handle)
-      build_entities_from_handles(:order, handles)
+      send("build_#{format}_array_from_handles", *[:order, handles])
     end
 
-    def get_current_invoices(debtor_handle)
+    def get_current_invoices(debtor_handle, format = :entitites)
       handles = fetch_handles(:get_current_invoices, debtor_handle)
-      build_entities_from_handles(:current_invoice, handles)
+      send("build_#{format}_array_from_handles", *[:current_invoice, handles])
     end
 
     private
 
-    def build_entities_from_handles(class_name, handles)
-      return nil if handles.nil?
-      camelized_name = class_name.to_s.split('_').map{|e| e.capitalize}.join
-      proxy = Economic.const_get("#{camelized_name}Proxy").new(owner)
-      proxy.get_data_array(handles).map! do |data|
+    def build_raw_array_from_handles(class_name, handles)
+      get_proxy_from_name(class_name).get_data_array(handles)
+    end
+
+    def build_hash_array_from_handles(class_name, handles)
+      get_proxy_from_name(class_name).get_data_array(handles).map! do |data|
+        data.to_hash
+      end
+
+    end
+
+    def build_entity_array_from_handles(class_name, handles)
+      get_proxy_from_name(class_name).get_data_array(handles).map! do |data|
         entity = proxy.build(data)
         entity.persisted = true
         entity.partial = false
         entity
       end
+    end
+
+    def get_proxy_from_name(class_name)
+      camelized_name = class_name.to_s.split('_').map{|e| e.capitalize}.join
+      Economic.const_get("#{camelized_name}Proxy").new(owner)
     end
 
     def fetch_handles(operation, debtor_handle)
