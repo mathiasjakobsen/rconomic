de8f1225aa30d32c50d8b17354b29993e0df37f3
Mathias Jakobsen <mathiasjakobsen@me.com>
2017-05-11 08:59:47 +0200

    Use full module scope, to get Entity module

--- lib/economic/proxies/actions/find_by_other_reference.rb
+++ lib/economic/proxies/actions/find_by_other_reference.rb
@@ -1,12 +1,10 @@
-require_relative './../../support/string'
-
 module FindByOtherReference
   def find_by_other_reference(other_reference)
     response = request(:find_by_other_reference, 'otherReference' => other_reference)
 
     handle_key = "#{Economic::Support::String.underscore(entity_class_name)}_handle".intern
     handles = [response[handle_key]].flatten.reject(&:blank?).collect do |handle|
-      Entity::Handle.build(handle)
+      Economic::Entity::Handle.build(handle)
     end
 
     get_data_array(handles).collect do |entity_hash|
